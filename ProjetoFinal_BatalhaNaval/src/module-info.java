7c4f9a0dba09f12311bb90ad5731b48e31d05b03
Ian Mendes <105458962+Ian-Mendes@users.noreply.github.com>
2024-06-07 12:00:26 -0300

    Update module-info.java
    Adição da opção para exportar o conteúdo do código em JavaFX.

--- ProjetoFinal_BatalhaNaval/src/module-info.java
+++ ProjetoFinal_BatalhaNaval/src/module-info.java
@@ -4,5 +4,12 @@
 /**
  * 
  */
-module ProjetoFinal_BatalhaNaval {
+module LP2_Batalha_Naval {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	exports br.ufrn.imd;
+
 }
